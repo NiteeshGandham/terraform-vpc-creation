ad7e3c15f84be76f3331ba442585dd22017cb5e5
Niteesh gandham <niteesh38@gmail.com>
2026-06-14 23:09:21 +0530

    terraform

--- vpc.tf
+++ vpc.tf
@@ -6,7 +6,7 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id #Association
 
   tags = local.vpc_final_tags
 
@@ -17,6 +17,25 @@ resource "aws_internet_gateway" "gw" {
 
  resource "aws_subnet" "public" {
    count = length(var.public_subnet_cidr)
+   vpc_id     = aws_vpc.main.id
+   cidr_block = var.public_subnet_cidr[count.index]
+   availability_zone = local.av_zone[count.index]
+   map_public_ip_on_launch = true
+
+   tags =  merge (
+      local.tags,
+      # roboshop-dev-public-us-east-1a
+      {
+        Name =  "${var.project}-${var.environment}-public-${local.av_zone[count.index]}"
+      },
+      var.subnet_tags
+
+     )
+   }
+
+#private subnets
+ resource "aws_subnet" "private" {
+   count = length(var.private_subnet_cidr)
    vpc_id     = aws_vpc.main.id
    cidr_block = var.public_subnet_cidr[count.index]
    availability_zone = local.av_zone[count.index]
