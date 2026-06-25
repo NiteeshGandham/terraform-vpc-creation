7cb7b329cc3cc8c107581cbc5f908c6880341fc7
Niteesh gandham <niteesh38@gmail.com>
2026-06-25 22:25:19 +0530

    terraform

--- data.tf
+++ data.tf
@@ -7,7 +7,7 @@ data "aws_vpc" "default" {
 }
 
 data "aws_route_tables" "default" {
-  vpc_id = data.aws_vpc_default.id
+  vpc_id = data.aws_vpc.default.id
 
   filter {
     name   = "association.main"
